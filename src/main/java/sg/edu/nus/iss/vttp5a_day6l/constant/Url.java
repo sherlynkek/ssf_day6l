d41cb3c82a242d447698db20a15da742bc09265b
sherlynkek <sherlynkek00@gmail.com>
2024-11-29 12:12:17 +0800

    country

--- src/main/java/sg/edu/nus/iss/vttp5a_day6l/constant/Url.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_day6l/constant/Url.java
@@ -4,6 +4,6 @@ public class Url {
 
     public static final String studentUrl = "http://localhost:3000/api/students";
 
-    public static final String countryUrl = "http://localhost:3000/api/country";
+    public static final String countryUrl = "http://https://api.first.org/data/v1/countries";
     
 }
